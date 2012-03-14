0cb8c082af58c09d961ce38e73637dc906ce1d8b
Kimbeaux <KimbeauxE@verizon.net>
2012-03-14 14:19:41 -0400

    Extra Credit 3

--- ex16.rb
+++ ex16.rb
@@ -22,12 +22,14 @@ print "line 3: "; line3 = STDIN.gets.chomp()
 
 puts "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+# target.write(line1)
+# target.write("\n")
+# target.write(line2)
+# target.write("\n")
+# target.write(line3)
+# target.write("\n")
+
+target.write("#{line1}\n#{line2}\n#{line3}\n")
 
 puts "And finally, we close it."
 target.close
